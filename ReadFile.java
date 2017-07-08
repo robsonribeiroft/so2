31c8753167bee7376264c4f5d9ce7292aabcb720
Robson Ribeiro <robsonribeiroft@gmail.com>
2017-07-08 12:07:43 -0300

    Update ReadFile.java

--- ReadFile.java
+++ ReadFile.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class ReadFile extends Thread {
 
-    private static final String FILE_LOCATION = "C:\\Users\\robso\\AndroidStudioProjects\\SistemaOperacionaisII\\src\\REFERENCIAS_100.txt";
+    private static final String FILE_LOCATION = "";
 
     ReadFile(){
         new Thread(this).start();
